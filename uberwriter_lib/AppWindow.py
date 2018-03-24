999b4bca4370234b739e7f187189555be8cf2129
somas95 <manuel.genoves@gmail.com>
2018-03-24 17:59:45 +0100

    Updated About dialog
    Signed-off-by: somas95 <manuel.genoves@gmail.com>

--- uberwriter_lib/AppWindow.py
+++ uberwriter_lib/AppWindow.py
@@ -157,7 +157,7 @@ class Application(Gtk.Application):
         about_dialog.set_copyright("Copyright (C) 2018, Wolf Vollprecht")
         about_dialog.set_license_type(Gtk.License.GPL_3_0)
         about_dialog.set_website("http://uberwriter.wolfvollprecht.de")
-        about_dialog.set_authors(["Wolf Vollprecht"])
+        about_dialog.set_authors(["Wolf Vollprecht", "Manuel Genovés"])
         
         logo_file = get_media_path("uberwriter.svg")
         logo = GdkPixbuf.Pixbuf.new_from_file(logo_file)
