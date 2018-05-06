23bde86ae1f344f1bcad30611c44efcd9d200411
somas95 <manuel.genoves@gmail.com>
2018-05-06 20:53:30 +0200

    updated version

--- uberwriter_lib/AppWindow.py
+++ uberwriter_lib/AppWindow.py
@@ -171,7 +171,7 @@ class Application(Gtk.Application):
     def on_about(self, action, param):
         about_dialog = Gtk.AboutDialog(transient_for=self.window, modal=True)
         about_dialog.set_program_name("Uberwriter")
-        about_dialog.set_version("2.0.beta")
+        about_dialog.set_version("2.0")
         about_dialog.set_copyright("Copyright (C) 2018, Wolf Vollprecht")
         about_dialog.set_license_type(Gtk.License.GPL_3_0)
         about_dialog.set_website("Uberwriter website http://uberwriter.wolfvollprecht.de")
