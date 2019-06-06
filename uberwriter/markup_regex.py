eec633437b96307d726112bbe9413e4e03800fe5
Gonçalo Silva <goncalossilva@gmail.com>
2019-06-06 02:57:43 +0100

    Improve inline preview
    - Uses commonly defined regexp
    - Removed dependency on gnome-web-photo, use WebView instead
    - Improved lexicon rendering

--- uberwriter/markup_regex.py
+++ uberwriter/markup_regex.py
@@ -9,22 +9,28 @@ BOLD_ITALIC = re.compile(
 STRIKETHROUGH = re.compile(
     r"~~(?P<text>.+?)~~")
 LINK = re.compile(
-    r"\[(?P<text>.*)\]\((?P<url>.+?)\)")
+    r"\[(?P<text>.*)\]\((?P<url>.+?)(?: \"(?P<title>.+)\")?\)")
+IMAGE = re.compile(
+    r"!\[(?P<text>.*)\]\((?P<url>.+?)(?: \"(?P<title>.+)\")?\)")
 HORIZONTAL_RULE = re.compile(
-    r"(?:^\n*|\n\n)(?P<symbols>[ ]{0,3}[*\-_]{3,}[ ]*)(?:\n+|$)")
+    r"(?:^\n*|\n\n)(?P<symbols> {0,3}[*\-_]{3,} *)(?:\n+|$)")
 LIST = re.compile(
-    r"(?:^\n*|\n\n)(?P<indent>(?:\t|[ ]{4})*)[\-*+]([ ]+)(?P<text>.+(?:\n+ \2.+)*)")
+    r"(?:^\n*|\n\n)(?P<indent>(?:\t| {4})*)[\-*+]( +)(?P<text>.+(?:\n+ \2.+)*)")
 ORDERED_LIST = re.compile(
-    r"(?:^\n*|\n\n)(?P<indent>(?:\t|[ ]{4})*)(?P<prefix>(?:\d|[a-z])+[.)]) (?P<text>.+(?:\n+ {2}\2.+)*)")
+    r"(?:^\n*|\n\n)(?P<indent>(?:\t| {4})*)(?P<prefix>(?:\d|[a-z])+[.)]) (?P<text>.+(?:\n+ {2}\2.+)*)")
 BLOCK_QUOTE = re.compile(
-    r"^[ ]{0,3}(?:> ?)+(?P<text>.+)", re.M)
+    r"^ {0,3}(?:> ?)+(?P<text>.+)", re.M)
 HEADER = re.compile(
-    r"^[ ]{0,3}(?P<level>#{1,6}) (?P<text>[^\n]+)", re.M)
+    r"^ {0,3}(?P<level>#{1,6}) (?P<text>[^\n]+)", re.M)
 HEADER_UNDER = re.compile(
-    r"(?:^\n*|\n\n)(?P<text>[^\s].+)\n[ ]{0,3}[=\-]+(?:\s+?\n|$)")
+    r"(?:^\n*|\n\n)(?P<text>[^\s].+)\n {0,3}[=\-]+(?:\s+?\n|$)")
 CODE_BLOCK = re.compile(
-    r"(?:^|\n)[ ]{0,3}(?P<block>([`~]{3})(?P<text>.+?)[ ]{0,3}\2)(?:\s+?\n|$)", re.S)
+    r"(?:^|\n) {0,3}(?P<block>([`~]{3})(?P<text>.+?) {0,3}\2)(?:\s+?\n|$)", re.S)
 TABLE = re.compile(
     r"^[\-+]{5,}\n(?P<text>.+?)\n[\-+]{5,}\n", re.S)
 MATH = re.compile(
     r"([$]{1,2})[^` ](?P<text>.+?)[^`\\ ]\1")
+FOOTNOTE_ID = re.compile(
+    r"[^\s]+\[\^(?P<id>[^\s]+)\]")
+FOOTNOTE = re.compile(
+    r"(?:^\n*|\n\n)\[\^(?P<id>[^\s]+)\]: (?P<text>(?:[^\n]+|\n+(?=(?:\t| {4})))+)(?:\n+|$)", re.M)
