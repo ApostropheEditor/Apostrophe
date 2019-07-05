213ff104e5689be7a8d17b52a8a8373feb5dd021
Manuel Genovés <manuel.genoves@gmail.com>
2019-07-05 20:47:49 +0200

    Update license

--- uberwriter/settings.py
+++ uberwriter/settings.py
@@ -1,5 +1,5 @@
 ### BEGIN LICENSE
-# Copyright (C) 2012, Wolf Vollprecht <w.vollprecht@gmail.com>
+# Copyright (C) 2019, Wolf Vollprecht <w.vollprecht@gmail.com>
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
