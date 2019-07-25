28446c42d1417cb017e63e27b0a5fe6a4e69f8aa
Gonçalo Silva <goncalossilva@gmail.com>
2019-07-26 00:01:01 +0100

    Move bin/uberwriter to uberwriter.in

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
         'uberwriter.pylocales' : ['locales.db'],
     },
     data_files=[
-        ('bin', ['bin/uberwriter']),
+        ('bin', ['uberwriter.in']),
         ('share/applications', ['data/de.wolfvollprecht.UberWriter.desktop']),
         ('share/metainfo', ['data/de.wolfvollprecht.UberWriter.appdata.xml']),
         ('share/icons/hicolor/scalable/apps', ['data/media/de.wolfvollprecht.UberWriter.svg']),
