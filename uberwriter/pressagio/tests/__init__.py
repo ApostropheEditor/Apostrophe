2fabd9a5000e8cb99229347b1a80a4f968aa2e6d
Manuel Genovés <manuel.genoves@gmail.com>
2020-02-02 13:29:00 +0100

    Remove deprecated pressagio package

--- uberwriter/pressagio/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Poio Tools for Linguists
-#
-# Copyright (C) 2009-2013 Poio Project
-# Author: Peter Bouda <pbouda@cidles.eu>
-# URL: <http://media.cidles.eu/poio/>
-# For license information, see LICENSE
