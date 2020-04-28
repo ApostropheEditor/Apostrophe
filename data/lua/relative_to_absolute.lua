bbdc07e3af22ab93f57b56afdb2c59fb446a22a1
Manuel Genovés <manuel.genoves@gmail.com>
2020-04-28 18:41:58 +0200

    Fix #222

--- data/lua/relative_to_absolute.lua
+++ data/lua/relative_to_absolute.lua
@@ -1,9 +1,13 @@
-function string.starts(String,Start)
-   return string.sub(String,1,string.len(Start))==Start
+function string.starts(String, Starts)
+  any_start_with = false
+  for i, Start in ipairs(Starts) do
+    any_start_with = any_start_with or string.sub(String,1,string.len(Starts[i]))==Starts[i]
+  end
+  return any_start_with
 end
 
 function fix_path (path)
-  if string.starts(path, "/") then
+  if string.starts(path, {"/", "www.", "http", "file://"}) then
     return path
   else
     return (pandoc.system.get_working_directory() or '') .. "/" .. path
@@ -12,10 +16,12 @@ end
 
 function Link (element)
   element.target = fix_path(element.target)
+  print(element.target)
   return element
 end
 
 function Image (element)
   element.src = fix_path(element.src)
+  print(element.src)
   return element
 end
