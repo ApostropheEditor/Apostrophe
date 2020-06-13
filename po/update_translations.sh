1a9c8074209afd8ac9f9702322c961e08460ca70
Manuel Genovés <manuel.genoves@gmail.com>
2020-06-13 22:18:25 +0200

    streamline l10n

--- po/update_translations.sh
+++ po/update_translations.sh
@@ -1,17 +1,3 @@
-function generate_po()
-{
-    >LINGUAS
-    for po in *.po
-    do
-        msgmerge -N $po uberwriter.pot > /tmp/$$language_new.po
-        mv /tmp/$$language_new.po $po
-        language=${po%.po}
-        echo $language >>LINGUAS
-    done
 
-    find . -name \*.po -execdir sh -c 'msgfmt "$0" -o uberwriter.mo' '{}' \; 
-
-}
-
-generate_po
-
+ninja -C builddir apostrophe-pot
+ninja -C builddir apostrophe-update-po
